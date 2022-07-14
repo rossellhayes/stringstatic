1bcddf7f2c99a011e7e2297ee7b8e742c564ef10
Alex Rossell Hayes <alexander@rossellhayes.com>
2022-07-14 11:28:06 -0700

    chore: use `*` to import all staticexports
    Requires wch/staticimports#19

--- R/import.R
+++ R/import.R
@@ -1,11 +1,2 @@
 # @staticimports inst/staticexports
-#   str_detect
-#   str_extract
-#   str_extract_all
-#   str_remove
-#   str_remove_all
-#   str_replace
-#   str_replace_all
-#   str_subset
-#   str_which
-#   fixed
+#  *
